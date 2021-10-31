74c7b05d8a51b93468a99358b77c3e47b5f4bd64
maddclif <maddclif24@gmail.com>
2021-10-31 19:55:16 +0300

    Finaly check

--- src/program.js
+++ src/program.js
@@ -1,12 +1,14 @@
 import { program } from 'commander/esm.mjs';
-import genDiff from '../src/genDiff.js';
+import genDiff from './genDiff.js';
 
 program
-    .description('Compares two configuration files and shows a difference.')
-    .version('1.0.0', '-V, --version', 'output the version number')
-    .option('-f, --format [type]', 'output format', 'stylish')
-    .arguments('<filepath1> <filepath2>')
-    .action((filepath1, filepath2) => console.log(genDiff(filepath1, filepath2, program.opts().format)))
-    .parse(process.argv);
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0', '-V, --version', 'output the version number')
+  .option('-f, --format [type]', 'output format', 'stylish')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2, program.opts().format));
+  })
+  .parse(process.argv);
 
 export default () => program;
