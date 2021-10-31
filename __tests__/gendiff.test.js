3a0ec5836bbae47531dadcd0ee5f09324629786d
maddclif <maddclif24@gmail.com>
2021-10-31 19:01:11 +0300

    Add json diff

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -30,3 +30,17 @@ test('plain diff yml', () => {
   const result = readFileSync(getPath('resultPlain.txt'), 'utf-8');
   expect(genDiff(filepath1, filepath2, 'plain')).toEqual(result);
 });
+
+test('json diff json', () => {
+  const filepath1 = getPath('file1.json');
+  const filepath2 = getPath('file2.json');
+  const result = readFileSync(getPath('resultJSON.txt'), 'utf-8');
+  expect(genDiff(filepath1, filepath2, 'json')).toEqual(result);
+});
+
+test('json diff yml', () => {
+  const filepath1 = getPath('file1.yml');
+  const filepath2 = getPath('file2.yml');
+  const result = readFileSync(getPath('resultJSON.txt'), 'utf-8');
+  expect(genDiff(filepath1, filepath2, 'json')).toEqual(result);
+});
--- src/formaters.js
+++ src/formaters.js
@@ -1,9 +1,11 @@
 import stylishRender from './stylishRender.js';
 import plainRender from './plainRender.js';
+import jsonRender from './jsonRender.js';
 
 const formatters = {
   default: stylishRender,
   plain: plainRender,
+  json: jsonRender,
 };
 
 export default (format) => formatters[format];
